a7d34a12ffc3b46cfda17f4ad57c57960f24141a
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 23:06:26 +0700

    Menambahkan borrowerRoute ke main route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bookRoute from './bookRoute.js';
 import authorRoute from './authorRoute.js';
 import categoryRoute from './categoryRoute.js';
+import borrowerRoute from './borrowerRoute.js';
 
 const router = express.Router();
 
 router.use('/books', bookRoute);
 router.use('/authors', authorRoute);
 router.use('/categories', categoryRoute);
+router.use('/borrowers', borrowerRoute);
 
 export default router;
