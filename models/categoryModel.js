4fa09e2977c1b9b44b31fcdd30ae5f90f799cd47
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 17:02:47 +0700

    Membuat schema category dengan collection category

---
+++ models/categoryModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model('category', categorySchema);
