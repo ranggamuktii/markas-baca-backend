31ea928b15ae8ecbb91017f502fd0a864b1ee010
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 22:09:29 +0700

    Membuat controller author

---
+++ controllers/authorController.js
@@ -0,0 +1,44 @@
+import Author from '../models/authorModel.js';
+
+export const getAuthors = async (req, res) => {
+  try {
+    const authors = await Author.find();
+    res.json(authors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getAuthorById = async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    if (!author) return res.status(404).json({ message: 'Author not found' });
+    res.status(200).json(author);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const addAuthor = async (req, res) => {
+  try {
+    const author = await Author.findById(req.body);
+    await author.save();
+    res.status(201).json(author);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateAuthor = async (req, res) => {
+  try {
+    const updatedAuthor = await Author.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!updatedAuthor) {
+      return res.status(404).json({ message: 'Author not found' });
+    }
+
+    res.status(200).json(updatedAuthor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
