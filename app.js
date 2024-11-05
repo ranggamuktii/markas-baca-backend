34dd327532c734bfe465b576e4fe29f1e2624ae9
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 21:45:44 +0700

    Mengimport cors pada server app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 import connectDB from './config/mongodb.js';
 
 dotenv.config();
@@ -7,6 +9,7 @@ const app = express();
 const port = process.env.PORT;
 
 connectDB();
+app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => console.log(`Server running on http://localhost: ${port}...`));
+app.listen(port, () => console.log(`Server running on http://localhost: ${port}`));
