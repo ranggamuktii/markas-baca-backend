b2a3f5fed3c9ef8ae6d5894be46574b4cb0acb94
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 23:00:27 +0700

    Menambahkan categoryRoute ke main route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bookRoute from './bookRoute.js';
 import authorRoute from './authorRoute.js';
+import categoryRoute from './categoryRoute.js';
 
 const router = express.Router();
 
 router.use('/books', bookRoute);
 router.use('/authors', authorRoute);
+router.use('/categories', categoryRoute);
 
 export default router;
