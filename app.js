ecc99839b02b81be08507e78b8ce1799218949e1
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 16:35:45 +0700

    Mengubah nama index.js menjadi app.js untuk main Express setup

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/mongodb.js';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+connectDB();
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server running on http://localhost: ${port}...`));
