4640831423bd9c67c0fc679d8bd9d48356a3e989
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 21:57:37 +0700

    Mengorganisir seluruh  endpoint dari routes/index.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import routes from './routes/index.js';
 import connectDB from './config/mongodb.js';
 
 dotenv.config();
@@ -11,5 +11,7 @@ const port = process.env.PORT;
 connectDB();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1', routes);
 
 app.listen(port, () => console.log(`Server running on http://localhost: ${port}`));
