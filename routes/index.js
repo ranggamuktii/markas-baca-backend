dd0630197eff17ef950b6ed09a0cd7d3682f2a7f
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-05 22:26:05 +0700

    Menambahkan authorRoute ke main route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bookRoute from './bookRoute.js';
+import authorRoute from './authorRoute.js';
 
 const router = express.Router();
 
 router.use('/books', bookRoute);
+router.use('/authors', authorRoute);
 
 export default router;
