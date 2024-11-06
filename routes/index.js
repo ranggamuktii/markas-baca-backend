07f475a0fa6f25738efb779a73713e2c0b1f20b7
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-06 21:15:12 +0700

    Menambahkan seluruh route dari main route agar lebih terorganisir

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ import authorRoute from './authorRoute.js';
 import categoryRoute from './categoryRoute.js';
 import borrowerRoute from './borrowerRoute.js';
 import borrowedRoute from './borrowedRoute.js';
+import stockLogRoutes from './stockLogRoute.js';
 
 const router = express.Router();
 
-router.use('/books', bookRoute);
-router.use('/authors', authorRoute);
-router.use('/categories', categoryRoute);
-router.use('/borrowers', borrowerRoute);
-router.use('/borrow', borrowedRoute);
+router.use('/', bookRoute);
+router.use('/', authorRoute);
+router.use('/', categoryRoute);
+router.use('/', borrowerRoute);
+router.use('/', borrowedRoute);
+router.use('/stockLog', stockLogRoutes);
 
 export default router;
